2775b9af9ee78c5db6a72dc7a93823816a083a59
Duong <duongtdn@gmail.com>
2016-11-30 06:21:00 +0700

    implementing edit menu

--- src/Todos/ListTab.js
+++ src/Todos/ListTab.js
@@ -1,22 +1,27 @@
 "use strict"
 
 import React , { Component } from 'react'
-import {Page, ListItem, List, Icon, Input, Col, Row} from 'react-onsenui'
+import {Page, ListItem, List, Icon, Input, Col, Row, Button} from 'react-onsenui'
 
 
 export default class extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {data : [
-      {role : 'Collaborator', text : 'Complete Todo App', dueDate : '30-Nov'},
-      {role : 'Owner', text : 'Study Admob', dueDate : '12-Dec'},
-      {role : 'Collaborator', text : 'Design using Onsen UI and build with Cordova', dueDate : ''},
-      {role : 'Owner', text : 'Find solutions for promoting app across multiple platform including Android, IOS, Web... or even third party vendors', dueDate : '15-Dec'}
-    ]};
+    this.state = {
+      data : [
+        {role : 'Collaborator', text : 'Complete Todo App', dueDate : '30-Nov'},
+        {role : 'Owner', text : 'Study Admob', dueDate : '12-Dec'},
+        {role : 'Collaborator', text : 'Design using Onsen UI and build with Cordova', dueDate : ''},
+        {role : 'Owner', text : 'Find solutions for promoting app across multiple platform including Android, IOS, Web... or even third party vendors', dueDate : '15-Dec'}
+      ],
+      showEditMenu : false
+    };
+    this.renderRow = this.renderRow.bind(this);
   }
 
   renderRow(row, index) {
+    const editMenuClass = this.state.showEditMenu ? 'todos-action' : 'todos-action hide';
     return (
       <ListItem key = {index} >
         <div className = 'left'> 
@@ -24,6 +29,10 @@ export default class extends Component {
         </div>
 
         <div className = 'center'>
+          <div className = {editMenuClass}>
+            <Button modifier='quiet'>  Edit <Icon icon = 'md-edit' /> </Button>
+            <Button modifier='quiet'> Share <Icon icon = 'md-share' /> </Button>
+          </div>
           <div className = 'todos-text'>
             {row.text}
           </div>
@@ -51,5 +60,9 @@ export default class extends Component {
         />
       </Page>
     );
+  }
+
+  toggleEditMenu() {
+
   }
 }
