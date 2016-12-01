02e3348fd259576801a6dc8dcf8dd0000b3f84fb
Duong <duongtdn@gmail.com>
2016-12-01 09:25:10 +0700

    add TaskInputs and taskOptions

--- src/TodoEditor/View.js
+++ src/TodoEditor/View.js
@@ -1,18 +1,50 @@
 "use strict"
 
 import React , { Component } from 'react'
-import { Page, List, ListItem, ListHeader } from 'react-onsenui'
+import { Page, List, ListItem, ListHeader, Input, Switch, Button } from 'react-onsenui'
 
 import Toolbar from './Toolbar'
 
 class TaskInputs extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    
+    return (
+      <List>
+
+        <ListHeader> Add a new Todo </ListHeader>
+
+        <ListItem modifier = 'nodivider'>
+          <div className = 'center'>
+            <Input type = 'text' placeholder = 'I want to...' float />
+          </div>
+        </ListItem>
+
+      </List>
+    );
   }
 }
 
 class TaskOptions extends Component {
+  render() {
+    return (
+      <List>
 
+        <ListHeader> Options </ListHeader>
+
+        <ListItem modifier = 'nodivider'>
+
+          <label className = 'center' > Urgent? </label>
+        
+          <label className = 'right' > <Switch /> </label>
+        
+        </ListItem>
+
+      </List>
+    );
+  }
 }
 
 export default class extends Component {
@@ -32,7 +64,8 @@ export default class extends Component {
      <Page renderToolbar = {this.renderToolbar}
             renderFixed = {this.renderFixed}
        >
-        <h3> Todo Editor Page </h3>
+        <TaskInputs />
+        <TaskOptions />
       </Page>
     );
   }
