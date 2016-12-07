be18fd8bbe46ea4c53d29f2877fab6794464a7bf
duongtdn <duongtdn@gmail.com>
2016-12-08 05:16:39 +0700

    start change View to COntainer

--- src/TodoEditor/View.js
+++ src/TodoEditor/View.js
@@ -3,10 +3,13 @@
 import React , { Component } from 'react'
 import { Page, Button, Icon} from 'react-onsenui'
 
+import { connect } from 'react-redux'
+import { todos } from 'todos-data'
+
 import Toolbar from './Toolbar'
 import TaskInputs from './TaskInputs'
 
-export default class extends Component {
+export default class TodoEditor extends Component {
   constructor(props) {
     super(props);
     this.renderToolbar = this.renderToolbar.bind(this);
@@ -23,12 +26,20 @@ export default class extends Component {
     const btn = this.props.data ? 'Save' : 'Add';
     return (
      <Page renderToolbar = {this.renderToolbar} >
-        <TaskInputs data = {this.props.data} pushPage = {this.props.pushPage} />
+        <TaskInputs data = {this.props.data} pushPage = {this.props.pushPage} getTodoText = {val => console.log(val)} />
         <div style={{padding: '16px'}}>
-          <Button ripple = {true} modifier = 'large' > {btn} </Button>
+          <Button ripple = {true} modifier = 'large' onClick = {() => { this.props.addTodo(todo) }} > {btn} </Button>
         </div>
       </Page>
     );
   }
 
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo(todo) {
+      dispatch(todos.add(todo));
+    }
+  }
+};
