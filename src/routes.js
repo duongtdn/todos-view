91f6116974cbb94d2224a4fbafafb54281f34e99
duongtdn <duongtdn@gmail.com>
2016-12-29 20:32:01 +0700

    cleanup removed modules

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ import React from 'react'
 import TodosView from './Todos/View'
 import TodoEditorView from './TodoEditor/View'
 import FriendsView from './Friends/View'
-import SearchView from './Search/View'
 import LoginView from './Login/View'
 import SignupView from './Signup/View'
 import SyncView from './Sync/View'
@@ -14,7 +13,6 @@ export default {
   todos : { name : 'todos', view : <TodosView /> },
   editor: { name : 'editor', view : <TodoEditorView /> },
   friends : { name : 'friends', view : <FriendsView /> },
-  search : { name : 'search', view : <SearchView />},
   login : { name : 'login', view : <LoginView />},
   signup : { name : 'signup', view : <SignupView />},
   sync : { name : 'sync', view : <SyncView />}
