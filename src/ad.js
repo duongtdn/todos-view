85d5cd3c15fb533b17bfa5a98ba4e8088a5b1dee
duongtdn <duongtdn@gmail.com>
2017-03-07 05:30:32 +0700

    premium account has no ad

--- src/ad.js
+++ src/ad.js
@@ -1,16 +1,25 @@
 "use strict"
 
+import { store } from 'todos-data'
 import app from './exporter'
 
 export default {
 
   loadBanner() {
+    const state = store.getState();
+    if (state.user && state.user.account && state.user.account === 'premium') {
+      return;
+    }
     if (app && app.plugin('ad')) {
       app.plugin('ad').showBottomBanner();
     }
   },
 
   loadInterstitial() {
+    const state = store.getState();
+    if (state.user && state.user.account && state.user.account === 'premium') {
+      return;
+    }
     if (app && app.plugin('ad')) {   
       app.plugin('ad').showInterstitial();
     }
